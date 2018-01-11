08d33dcf1d2e5284490cebc28aa3c50c8fb54b92
nichita <nichita@cadvegra.com>
2018-01-11 21:01:35 +0200

    added sqrt calculator function

--- sqrt.js
+++ sqrt.js
@@ -1,3 +1,16 @@
 function calcSqrt(num){
+    var s = Math.sqrt(num)
+    var count = 0;
+    var res = "";
 
+    for(var i = 2 ; i<=num ; i++){
+        for(count = 0; num % i == 0 ; count++){
+            num /=i;
+        }
+        if(count > 0){
+            res += i + "^" + count + "\n";
+        }
+    }
+
+    return res;
 }
